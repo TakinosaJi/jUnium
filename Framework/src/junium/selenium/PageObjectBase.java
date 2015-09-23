79d7e9ddd99c9b1b361b331264d6c3dd81c5607e
Triss <Anna.Abalmaz@gmail.com>
2015-09-23 19:33:58 +0300

    Lazy loading of page objects implemented.

--- Framework/src/junium/selenium/PageObjectBase.java
+++ Framework/src/junium/selenium/PageObjectBase.java
@@ -3,6 +3,7 @@ package junium.selenium;
 import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import junium.exceptions.PageObjectNotFoundException;
 import junium.selenium.interfaces.IWebDriverAccessible;
+import junium.selenium.annotations.LazyLoading;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,24 +14,31 @@ public class PageObjectBase extends WebElementsContainer implements IWebDriverAc
     public String Name;
     private String MarkerXPath;
     private WebElement Marker;
+    private Boolean _lazyLoading;
+
+    public PageObjectBase(WebDriver driver, WebDriverWait wait, String name, String markerXPath, Boolean lazyLoading) throws PageObjectNotFoundException {
+        super(driver, wait);
+        Init(name, markerXPath, lazyLoading);
+    }
 
     public PageObjectBase(WebDriver driver, WebDriverWait wait, String name, String markerXPath) throws PageObjectNotFoundException {
         super(driver, wait);
-
-        Name = name;
-        MarkerXPath = markerXPath;
-
-        exists();
+        Init(name, markerXPath, this.getClass().isAnnotationPresent(LazyLoading.class));
     }
 
-    public WebElement getMarker(){
-        return Marker;
+    private void Init(String name, String markerXPath, Boolean lazyLoading) throws PageObjectNotFoundException {
+        Name = name;
+        MarkerXPath = markerXPath;
+        _lazyLoading = lazyLoading;
+        if(!_lazyLoading) {
+            getMarker();
+        }
     }
 
     @Override
-    public void exists() throws PageObjectNotFoundException {
+    public WebElement getMarker() throws PageObjectNotFoundException{
         try {
-            Marker = Wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(MarkerXPath)));
+           return Marker = Wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(MarkerXPath)));
         }
         catch (ElementNotFoundException e){
             throw new PageObjectNotFoundException(Name);
--- Framework/src/junium/selenium/interfaces/IWebDriverAccessible.java
+++ Framework/src/junium/selenium/interfaces/IWebDriverAccessible.java
@@ -2,7 +2,8 @@ package junium.selenium.interfaces;
 
 
 import junium.exceptions.PageObjectNotFoundException;
+import org.openqa.selenium.WebElement;
 
 public interface IWebDriverAccessible {
-    public void exists() throws PageObjectNotFoundException;
+    public WebElement getMarker() throws PageObjectNotFoundException;
 }
